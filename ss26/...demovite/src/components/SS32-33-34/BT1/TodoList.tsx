88b893a0b259f8588353ef0f8177590d649e6c6f
HuynhTamAnh <huynhtamanh2000@gmail.com>
2024-06-14 08:22:46 +0700

    1

--- ss26/...demovite/src/components/SS32-33-34/BT1/TodoList.tsx
+++ ss26/...demovite/src/components/SS32-33-34/BT1/TodoList.tsx
@@ -15,12 +15,13 @@ const colors = [
 type Todo = {
   id: number;
   title: string;
-  // text: string;
   status: boolean;
 };
 
 type StateType = {
   todos: Todo[];
+  editId: number | null;
+  editTitle: string;
 };
 
 export default class TodoList extends Component<{}, StateType> {
@@ -31,13 +32,15 @@ export default class TodoList extends Component<{}, StateType> {
         { id: 1, title: "học toán", status: false },
         { id: 2, title: "học code", status: true },
       ],
+      editId: null,
+      editTitle: "",
     };
   }
 
   changeStatus = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let id = e.target.value as number | unknown;
+    let id = parseInt(e.target.value, 10);
     let newTodos = this.state.todos.map((todo) => {
-      if (todo.id == id) {
+      if (todo.id === id) {
         todo.status = !todo.status;
       }
       return todo;
@@ -47,6 +50,7 @@ export default class TodoList extends Component<{}, StateType> {
       todos: newTodos,
     });
   };
+
   handleDelete = (id: number) => {
     if (window.confirm("chắc chưa?")) {
       let newTodos = this.state.todos.filter((todo) => todo.id !== id);
@@ -55,6 +59,33 @@ export default class TodoList extends Component<{}, StateType> {
         todos: newTodos,
       });
     }
+  };
+
+  handleEdit = (id: number, title: string) => {
+    this.setState({
+      editId: id,
+      editTitle: title,
+    });
+  };
+
+  handleEditChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      editTitle: e.target.value,
+    });
+  };
+
+  saveEdit = (id: number) => {
+    let newTodos = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.title = this.state.editTitle;
+      }
+      return todo;
+    });
+    this.setState({
+      todos: newTodos,
+      editId: null,
+      editTitle: "",
+    });
   };
 
   render() {
@@ -84,16 +115,38 @@ export default class TodoList extends Component<{}, StateType> {
                     value={todo.id}
                     onChange={this.changeStatus}
                   />
-                  <span
-                    className={
-                      todo.status ? "text-decoration-line-through" : ""
-                    }
-                  >
-                    {todo.title}
-                  </span>
+                  {this.state.editId === todo.id ? (
+                    <InputGroup className="me-2">
+                      <Form.Control
+                        value={this.state.editTitle}
+                        onChange={this.handleEditChange}
+                      />
+                      <Button
+                        variant="success"
+                        onClick={() => this.saveEdit(todo.id)}
+                      >
+                        Save
+                      </Button>
+                    </InputGroup>
+                  ) : (
+                    <span
+                      className={
+                        todo.status ? "text-decoration-line-through" : ""
+                      }
+                    >
+                      {todo.title}
+                    </span>
+                  )}
                 </div>
                 <div>
-                  <Button variant="warning me-3">Sửa</Button>
+                  {this.state.editId === todo.id ? null : (
+                    <Button
+                      variant="warning me-3"
+                      onClick={() => this.handleEdit(todo.id, todo.title)}
+                    >
+                      Sửa
+                    </Button>
+                  )}
                   <Button
                     variant="danger"
                     onClick={() => this.handleDelete(todo.id)}
